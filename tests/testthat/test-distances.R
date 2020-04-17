ee5b1d799b74b097c34f641457fd3791332e8311
Konstantin Krismer <krismer@mit.edu>
2020-04-17 18:44:01 -0400

    Add tests for distance functions

--- tests/testthat/test-distances.R
+++ tests/testthat/test-distances.R
@@ -43,3 +43,31 @@ test_that("calculate_midpoint_distance2d", {
                                                    c(260, 270, 240, 260)),
                      c(0L, 0L, 20L, 10L))
 })
+
+test_that("calculate_relative_overlap1d", {
+    expect_equal(calculate_relative_overlap1d(100, 120, 100, 120), 1.0)
+    expect_equal(calculate_relative_overlap1d(100, 120, 100, 110), 0.5)
+    expect_equal(calculate_relative_overlap1d(100, 120, 130, 140), -0.25)
+    expect_equal(calculate_relative_overlap1d(100, 120, 200, 220), -0.666666,
+                 tolerance = .00002)
+
+    expect_equal(calculate_relative_overlap1d(c(100, 100, 100, 100),
+                                              c(120, 120, 120, 120),
+                                              c(100, 100, 130, 200),
+                                              c(120, 110, 140, 220)),
+                 c(1.0, 0.5, -0.25, -0.666666),
+                 tolerance = .00002)
+})
+
+test_that("calculate_midpoint_distance1d", {
+    expect_identical(calculate_midpoint_distance1d(100, 120, 100, 120), 0L)
+    expect_identical(calculate_midpoint_distance1d(0, 0, 100, 120), 110L)
+    expect_identical(calculate_midpoint_distance1d(100, 120, 90, 130), 0L)
+    expect_identical(calculate_midpoint_distance1d(100, 120, 110, 130), 10L)
+    expect_identical(calculate_midpoint_distance1d(100, 120, 90, 200), 35L)
+    expect_identical(calculate_midpoint_distance1d(c(100, 100, 100, 100),
+                                                   c(120, 120, 120, 120),
+                                                   c(100, 90, 110, 90),
+                                                   c(120, 130, 130, 200)),
+                     c(0L, 0L, 10L, 35L))
+})
