da77a9980ea81f5b96de0bc4cd1bc0d60f7eeff7
Konstantin Krismer <krismer@mit.edu>
2020-04-18 11:16:28 -0400

    Add Hi-C summary tests

--- tests/testthat/test-hic.R
+++ tests/testthat/test-hic.R
@@ -3,6 +3,7 @@ library(idr2d)
 
 test_that("estimate_idr2d_hic", {
     futile.logger::flog.threshold(futile.logger::WARN)
+    set.seed(3)
     idr_results_df <- estimate_idr2d_hic(idr2d:::hic$rep1_df,
                                          idr2d:::hic$rep2_df)
 
@@ -11,4 +12,21 @@ test_that("estimate_idr2d_hic", {
                                              "rep_value", "rank",
                                              "rep_rank", "idr"))
     expect_equal(sum(idr_results_df$value), 5025075)
+    expect_equal(sum(idr_results_df$idr), 7.262495,
+                 tolerance = 0.2)
+
+    summarized_results <- summary(idr_results_df)
+    expect_equal(summarized_results$analysis_type, "IDR2D Hi-C")
+    expect_equal(summarized_results$num_blocks, 3081)
+    expect_equal(summarized_results$num_significant_blocks, 3063)
+    expect_equal(summarized_results$num_highly_significant_blocks, 3022)
+
+
+    idr_results_df <- estimate_idr2d_hic(idr2d:::hic$rep1_df,
+                                         idr2d:::hic$rep2_df, local_idr = TRUE,
+                                         combined_max_value = 10^7,
+                                         min_value = -20,
+                                         max_value = 10^7)
+    expect_equal(sum(idr_results_df$idr), 7.276815,
+                 tolerance = 0.2)
 })
